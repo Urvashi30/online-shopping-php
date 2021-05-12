2ac7599e9ad1047b2da16318ea837f7d0848fd57
Urbashi Jrall <60031536+Urvashi30@users.noreply.github.com>
2021-05-12 23:19:02 +0530

    I made a project on Online Shopping Cart and Checkout Website using HTML, CSS, BOOTSTRAP, PHP, MYSQL
    I made a project on Online Shopping Cart and Checkout Website using HTML, CSS, BOOTSTRAP, PHP, MYSQL

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>     
-    <div class="container">         
-                   
-            <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</p>          
-          
-    </div> 
-</footer>
--- php assignment solution/assignment 3/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
-//$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
-?>
-
-
-
